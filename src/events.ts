e4208d7914c9804945dd1af48639b489d38cd49b
Nahuel <nanocode2022@gmail.com>
2023-08-21 12:07:36 -0300

    add: put method to edit events

--- src/events.ts
+++ src/events.ts
@@ -8,7 +8,7 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   const filters = req.query
-  const filteredEvents = getEvents().filter((data:any):boolean => {
+  const filteredEvents = getEvents().filter((data: any): boolean => {
     let isValid = true
     for (let key in filters) {
       isValid = isValid && data[key].toLowerCase().includes(filters[key])
@@ -18,4 +18,14 @@ router.get('/', (req, res) => {
   res.send(filteredEvents)
 })
 
+router.put('/edit/:id', (req, res) => {
+  const data = req.body
+  const id = req.params.id
+  const indice = getEvents().findIndex(value => value.name == id)
+
+  if (indice >= 0) {
+    getEvents()[indice] = data
+  }
+  res.status(200).send(getEvents())
+})
 export default router
